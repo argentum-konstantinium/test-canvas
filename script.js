edc99ecfecc395a8497c555557f8f3978773ea9c
argentum-konstantinium <64431957+argentum-konstantinium@users.noreply.github.com>
2021-03-18 21:12:12 +0300

    Update script.js

--- script.js
+++ script.js
@@ -4,13 +4,14 @@ const RENDERER = new THREE.WebGLRenderer({
     antialias: true
 });
 const loader = new THREE.TextureLoader();
-
+var light = new THREE.HemisphereLight( 0xffffbb, 0x887979, 2.6);
+SCENE.add( light );
 texture = loader.load('./bg.png');
 texture.wrapS = THREE.RepeatWrapping;
 texture.wrapT = THREE.RepeatWrapping;
-texture.repeat.set( 40, 40 );
+texture.repeat.set( 30, 30 );
 texture.anisotropy = 2;
-
+ 
 rendererResizer();
 window.addEventListener('resize', rendererResizer);
 
@@ -21,48 +22,53 @@ function rendererResizer() {
 
 document.body.appendChild(RENDERER.domElement);
 
-const GEOMETRY = new THREE.PlaneBufferGeometry(-100, -100, 100, 100);
-const MATERIAL = new THREE.MeshBasicMaterial({
-   
+const GEOMETRY = new THREE.PlaneBufferGeometry(-100, -100, 150, 150);
+const MATERIAL = new THREE.MeshStandardMaterial({
+     
     map: texture,
-   // side: THREE.DoubleSide,
-   // wireframe: true,
-});
+    
+ 
+    
+})
 const PLANE = new THREE.Mesh(GEOMETRY, MATERIAL);
 PLANE.scale.x = 3;
 PLANE.scale.y = 3;
 
-PLANE.rotation.x = -1;
+PLANE.rotation.x = -1.2;
+ 
 
 SCENE.add(PLANE);
 let positionAttribute = GEOMETRY.attributes.position;
 
 CAMERA.position.z = 10;
-
+ a = 0.01;
 
 function updateVerticles() {
-    offset = Date.now() * 0.001;
+    offset = Date.now() * 0.0007;
  
     for (let i = 0; i < positionAttribute.count; i++) {
         let x = positionAttribute.getX(i);
         let y = positionAttribute.getY(i);
-        z = positionAttribute.getZ(i);
+ 
       
-        z = noise.simplex2(x / 5, y / 5 + offset) * 1.2;
-        positionAttribute.setXYZ(i, x, y, z);
+        z = noise.simplex2(x / 5, y / 5 + offset) * 1.3;
+        positionAttribute.setZ(i, z);
        
-         
-        
+        texture.offset.y -= a;
+        if (texture.offset.y <= -20) {
+            texture.offset.y = 0;
+        };
+       
         GEOMETRY.attributes.position.needsUpdate = true;
        
     };
 };
 function animate() {
 
-
-    updateVerticles();
-    GEOMETRY.attributes.position.needsUpdate = true;
     requestAnimationFrame(animate);
     RENDERER.render(SCENE, CAMERA);
+    updateVerticles();
+   
+   
 };
 animate();
